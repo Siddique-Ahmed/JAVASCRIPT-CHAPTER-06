728d6366970c2c83833a86418f5bb0b306b17b69
Siddique-Ahmed <itssiddique786@gmail.com>
2024-03-09 15:02:54 -0800

    chapter 6

---
+++ script.js
@@ -0,0 +1,31 @@
+var a = 10;
+document.write("The value of a is : "+a)
+document.write("<br>")
+a++;
+document.write("The value of ++a is : " + a)
+document.write("<br>")
+document.write("Now the value of a is : "+ a)
+document.write("<br>")
+document.write("The value of a++ is : " + a)
+document.write("<br>");
+a++;
+document.write("The value of a++ is : " + a++)
+document.write("<br>") 
+a--;
+document.write("The value of a-- is : " + --a)
+document.write("<br>")
+document.write("Now the value of --a is : "+ a--)
+document.write("<br>")
+a++;
+document.write("The value of --a is :" + a++)
+document.write("<br>")
+a--;
+document.write("Now the value of --a is : " + --a)
+
+// ################### User Table ######################
+
+document.write("<table>")
+var math
+
+
+document.write("</table>")
